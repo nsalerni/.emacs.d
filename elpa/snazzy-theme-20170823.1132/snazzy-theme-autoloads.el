3d6986f7c643e2c4beab9dbccc6ff0cd8ab91f0f
Nick Salerni <nsalerni@amazon.com>
2018-06-11 15:30:57 -0400

    Added two new themes

---
+++ elpa/snazzy-theme-20170823.1132/snazzy-theme-autoloads.el
@@ -0,0 +1,19 @@
+;;; snazzy-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "snazzy-theme" "snazzy-theme.el" (23326 51259
+;;;;;;  0 0))
+;;; Generated autoloads from snazzy-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; snazzy-theme-autoloads.el ends here
